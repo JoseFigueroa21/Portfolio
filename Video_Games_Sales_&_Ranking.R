cb389acb63d37298a0a75f8c54844503674e099a
JoseFigueroa21 <75599007+JoseFigueroa21@users.noreply.github.com>
2023-05-19 16:16:41 -0700

    Update Video_Games_Sales_&_Ranking.R

--- Video_Games_Sales_&_Ranking.R
+++ Video_Games_Sales_&_Ranking.R
@@ -1,16 +1,10 @@
-## Jose Figueroa
-## Math 2200
-## 11/20/2022
-
 require(openintro)
 require(mosaic)
 attach(stats_data)
 
-## Graphs used
 histogram(~GlobalSales, data = stats_data)
 qqnorm(GlobalSales)
 plot(GlobalSales~Rank, data = stats_data)
 
-## additional code
 Rank.GlobalSales.lm <- lm(GlobalSales~Rank, data = stats_data)
 summary(Rank.GlobalSales.lm)
